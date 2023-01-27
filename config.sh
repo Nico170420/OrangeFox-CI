0e55d811b918180b018beac257766468dd77e546
Nico1704 <b1704nc@gmail.com>
2023-01-27 10:37:44 +0100

    Adopt for S20

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://github.com/Nico170420/android_device_samsung_z3s -b twrp"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="z3s"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
@@ -17,12 +17,12 @@ export OUTPUT="OrangeFox*.zip"
 
 # Additional Dependencies (eg: Kernel Source)
 # Format: "repo dest"
-DEPS=(
-    "https://github.com/OrangeFoxRecovery/Avatar.git misc"
-)
+#DEPS=(
+#    "https://github.com/OrangeFoxRecovery/Avatar.git misc"
+#)
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=Nico1704"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
